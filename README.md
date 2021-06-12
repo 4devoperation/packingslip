650915874ee2b95d0eea8260b3ca946c759526e4
plainspeak <49394636+plainspeak@users.noreply.github.com>
2021-06-12 19:11:36 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# packing_slip
+ 
---
+++ android/app/src/main/java/com/example/packing_plip/MainActivity.java
@@ -0,0 +1,6 @@
+package com.example.packing_plip;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
